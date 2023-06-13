973757f8e6657a2e234315d0881f5b4bab70b31c
dannhu283 <dannhu2803@gmail.com>
2023-06-13 20:50:27 +0700

    add

--- 9_loop/index.js
+++ 9_loop/index.js
@@ -54,3 +54,134 @@ document.getElementById("btn-2").onclick = () => {
   outputEl.innerHTML = total;
   outputEl.classList.remove("d-none");
 };
+
+// 3. vòng lặp for
+for (let i = 1; i <= 5; i++) {
+  console.log("hello DN");
+}
+
+// bài tập 3: in tất cả số nguyên dương lẻ nhỏ hơn 100
+function ex3() {
+  let output = "";
+  for (let j = 1; j <= 100; j++) {
+    if (j % 2 !== 0) {
+      output += j + ",";
+    }
+  }
+  // output=" 1,3,5,....99"
+  console.log("ex3:", output);
+}
+ex3();
+//4.vòng lặp lồng nhau
+let count = 0;
+for (let i = 1; i <= 5; i++) {
+  for (let j = 1; j <= 5; j++) {
+    count++;
+  }
+}
+console.log(" số lần chạy:", count);
+
+//bài tập in ra hình vuông dâu * có kích thước 5*5
+function ex4() {
+  let output = "";
+  for (i = 1; i <= 5; i++) {
+    for (j = 1; j <= 5; j++) {
+      output += "* ";
+    }
+    output += "\n";
+  }
+  console.log(output);
+}
+ex4();
+
+// dùng vòng lặp để duyệt qua tưng kí tự trong chuỗi
+let message = "Hello Cybersoft";
+// cách truy cập vào 1 kí tự bất kì trong chuối dựa vào chỉ mục
+console.log(message[0]); //H
+console.log(message[1]); //e
+// lấy ra độ dài của chuỗi
+console.log(message.length);
+for (let i = 0; i < message.length; i++) {
+  console.log(message[i]);
+}
+
+// lệnh break(thoát khỏi vòng lặp)
+// bài tập 5: cộng giá trị từ 1 đến n bất kì cho đến khi tổng các số đó vượt quá 100 thì dừng lại
+function ex5() {
+  let i = 1;
+  let sum = 0;
+  while (true) {
+    sum += i;
+    if (sum > 100) {
+      break;
+    }
+    i++;
+  }
+  console.log(sum, i);
+}
+ex5();
+// lệnh continue
+function ex6() {
+  let total = 0;
+  for (let i = 1; i <= 10; i++) {
+    if (i === 4) {
+      continue;
+    }
+    total += i;
+  }
+  console.log(total);
+}
+ex6();
+
+// lệnh return( thoát khỏi cả vòng lặp và hàm)
+function ex7() {
+  for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+      return i;
+    }
+    console.log(i);
+  }
+  console.log("run run run");
+}
+ex7();
+
+//bài tập viết hàm kiểm tra 1 số có phải là số nguyên tố hay không
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+  if (n % 2 === 0) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log("isPrime(5)", isPrime(5));
+console.log("isPrime(10)", isPrime(10));
+isPrime(5); // true    vì 5 là sô nguyên tố
+isPrime(10); //false    10 không phải là SNT
+//cách tối ưu hơn
+// function isPrime(n) {
+//   if (n < 2) {
+//     return false;
+//   }
+//   if (n === 2) {
+//     return true;
+//   }
+//   if (n % 2 === 0) {
+//     return false;
+//   }
+//   for (let i = 3; i <= Math.sqrt(n); i += 2) {
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
